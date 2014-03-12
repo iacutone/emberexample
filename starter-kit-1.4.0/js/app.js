3cee19b03815daf81b197e1fdd61af8269e72928
Eric Iacutone <eric.iacutone@gmail.com>
2014-03-12 00:25:35 -0400

    almost done with routing tutorial

--- starter-kit-1.4.0/js/app.js
+++ starter-kit-1.4.0/js/app.js
@@ -2,12 +2,35 @@ App = Ember.Application.create({
   LOG_TRANSITIONS: true, // basic logging of successful transitions
 });
 
-App.Router.map(function() {
-  // put your routes here
+App.Song = Ember.Object.extend({
+  title: null,
+  rating: null,
+  artist: null
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.Artist = Ember.Object.extend({
+  name: null
 });
+
+var artistNames = ['Led Zeppelin', 'Black Moon'];
+
+App.Artists = artistNames.map(function(name){ 
+	return App.Artist.create({ name: name });
+});
+
+App.Songs = Ember.A();
+
+App.Songs.pushObject(App.Song.create({ title: 'Black Dog', artist: 'Led Zeppelin', rating: 8 }));
+App.Songs.pushObject(App.Song.create({ title: 'Talk Shit', artist: 'Black Moon', rating: 10 }));
+
+App.Router.map(function(){
+	this.route('artists', { path: '/artists'} );
+});
+
+App.ArtistsRoute = Ember.Route.extend({
+	model: function(){
+		return App.Artists;
+	}
+});
+
+
